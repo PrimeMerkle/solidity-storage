830b458628999e62441f369d74cdf5ece2129bc7
Prime Merkle <124616944+PrimeMerkle@users.noreply.github.com>
2026-08-27 00:58:59 +0100

    Add Storage contract with store and retrieve functions

---
+++ Storage.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.18;
+
+contract Storage {
+    uint256 private number;
+
+    function store(uint256 _number) public {
+        number = _number;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
